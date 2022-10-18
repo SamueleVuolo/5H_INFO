e3df32bf86175c9d0913bca1f77218c37ebda6e1
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2022-10-18 13:04:09 +0200

    Add files via upload

---
+++ ES02/PagRiservata.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>ITCS ERASMO DA ROTTERDAM</title>
+</head>
+<body>
+	<h4>Richiesta credenziali per l'accesso all'area riservata</h4><br/>
+	
+	<form name="frmLogin" action="login.php" method="POST">
+		User name: <input type="text" name="username"><br>
+		Password: <input type="password" name="password"><br>
+		<input type="submit" value="Submit">
+	</form>
+	
+</body>
+</html>
