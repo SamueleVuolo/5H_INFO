fb18aa0829a1ee57c6a586e511d8845fc11dc13e
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2022-10-20 12:00:48 +0200

    Versione 1.0

---
+++ PHP/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Home Page</title>
+</head>
+<body>
+<h1>Home page - Vuolo Samuele</h1><hr/>
+<a href="info5H_vuolo/ES01-TavPit/TavolaPit_Vuolo5H.php"><h2>Es01_Tavola</a><br/></h2>
+<a href="info5H_vuolo/ES02_A-Form/PagRiservata.php"><h2>Es02_Login</a><br/></h2>
+<a href="info5H_vuolo/ES02_B-FormModificato/LoginUnico.php"><h2>Es03_LoginUnico</a><br/></h2>
+<hr/>
+
+</body>
+</html>
