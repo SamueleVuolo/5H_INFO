5234590730703c2707176342dd2bc73d8f23b0aa
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2022-11-10 12:04:30 +0100

    v1.0

---
+++ PHP/ES03/ValidazioneForm.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>ITCS ERASMO DA ROTTERDAM</title>
+</head>
+<body>
+	<h2></h4><hr/>
+	
+	<form name="frmLogin" action="login.php" method="POST">
+		Nome: <input type="text" name="nome"><br>
+		Cognome: <input type="text" name="cognome"><br>
+		Data di nascita: <input type="date" name="data"><br>
+		Nickname: <input type="text" name="nickname"><br>
+		Email: <input type="text" name="email"><br>
+		Cellulare: <input type="text" name="cellulare"><br>
+		Password: <input type="password" name="password" placeholder="password"><br>
+		<input type="submit" value="Submit">
+	</form><hr/>
+	
+</body>
+</html>
