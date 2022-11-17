1c6fff8e4f506a94285fb61b40c812cdc1c113fc
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2022-11-17 12:02:08 +0100

    Update check.php

--- PHP/ES03/check.php
+++ PHP/ES03/check.php
@@ -1,5 +1,8 @@
+<html>
+<head>
+	<link rel="stylesheet" href="stile.css"/>
+</head>
 <?php
-
 $nome=trim($_POST["nome"]);
 $cognome=trim($_POST["cognome"]);
 $data=trim($_POST["data"]);
@@ -28,7 +31,7 @@ else
 		echo "<br>Caratteri non accettati, inserire un nuovo cognome. <br>";
 	}
 
-if(preg_match("^[0-3][0-9]-[0-1][0-9]-[0-9]{4}$",$data))
+/*if(preg_match("[0-3][0-9]-[0-1][0-9]-[0-9]{4}$",$data))
 	{
 		echo "<br>Data di nascita inserita: ";
 		echo $data;
@@ -36,7 +39,7 @@ if(preg_match("^[0-3][0-9]-[0-1][0-9]-[0-9]{4}$",$data))
 else
 	{
 		echo "<br>Data non accettata, inserire una nuova data. <br>";
-	}
+	}*/
 
 if(preg_match('/^[a-zA-Z0-9_]*$/',$nickname))
 	{
@@ -46,5 +49,35 @@ if(preg_match('/^[a-zA-Z0-9_]*$/',$nickname))
 else
 	{
 		echo "<br>Caratteri non accettati, inserire un nuovo nickname. <br>";
+	}
+	
+if(preg_match('/^[a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/',$email))
+	{
+		echo "<br>Email inserita: ";
+		echo $email;
+	}
+else
+	{
+		echo "<br>Caratteri non accettati, inserire un nuovo nickname. <br>";
+	}
+	
+if(preg_match('/^[0-9]/',$cellulare))
+	{
+		echo "<br>Numero di cellulare inserito: ";
+		echo $cellulare;
+	}
+else
+	{
+		echo "<br>Caratteri non accettati, inserire un nuovo numero. <br>";
+	}
+	
+if(preg_match('/^[a-zA-Z_0-9]/',$password))
+	{
+		echo "<br>Numero di cellulare inserito: ";
+		echo $password;
+	}
+else
+	{
+		echo "<br>Caratteri non accettati, inserire una nuova password. <br>";
 	}
 ?>
