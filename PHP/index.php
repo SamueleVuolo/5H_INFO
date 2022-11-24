ffee021ba1dc89f875a5086c37e3226487d83568
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2022-11-24 10:26:12 +0100

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -4,9 +4,10 @@
 </head>
 <body>
 <h1>Home page - Vuolo Samuele</h1><hr/>
-<a href="info5H_vuolo/ES01-TavPit/TavolaPit_Vuolo5H.php"><h2>Es01_Tavola</a><br/></h2>
-<a href="info5H_vuolo/ES02_A-Form/PagRiservata.php"><h2>Es02_Login</a><br/></h2>
-<a href="info5H_vuolo/ES02_B-FormModificato/LoginUnico.php"><h2>Es03_LoginUnico</a><br/></h2>
+<a href="info5H_vuolo/ES01 - Tavola Pitagorica/TavolaPit_Vuolo5H.php"><h2>Es01_Tavola</a><br/></h2>
+<a href="info5H_vuolo/ES02_A - Accesso a pagina riservata/PagRiservata.php"><h2>Es02_B_Login</a><br/></h2>
+<a href="info5H_vuolo/ES02_B - Login Unico/LoginUnico.php"><h2>Es02_A_LoginUnico</a><br/></h2>
+<a href="info5H_vuolo/ES03 - Validazione Form/FormValidazione.php"><h2>Es03_Validazione</a><br/></h2>
 <hr/>
 
 </body>
