1283f0141a906a64e7f4328b8e85c24bcfe037c7
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2022-11-28 18:54:05 +0100

    Update FormValidazione.php

--- PHP/ES03/FormValidazione.php
+++ PHP/ES03/FormValidazione.php
@@ -27,89 +27,107 @@ if(isset($_POST["nome"])){
 }
 
 //controllo campi
+echo "Nome: ";
 if(preg_match('/^[a-zA-Z]*$/',$nome))
 	{
-		echo "Nome inserito: ";
-		echo $nome;
+		echo $nome, "<br>";
 	}
 else
 	{
-		echo $errNome = "Caratteri non accettati, inserire un nuovo nome. <br>";
+		echo $errNome = "nome invalido, inserire un nuovo nome. <br>";
 	}
 
+echo "Cognome: ";
 if(preg_match('/^[a-zA-Z]*$/',$cognome))
 	{
-		echo "<br>Cognome inserito: ";
-		echo $cognome;
+		echo $cognome, "<br>";
 	}
 else
 	{
-		echo $errCognome = "Caratteri non accettati, inserire un nuovo cognome. <br>";
+		echo $errCognome = "cognome invalido, inserire un nuovo cognome. <br>";
 	}
+	
 
-/*if(preg_match("[0-3][0-9]-[0-1][0-9]-[0-9]{4}$",$data))
+/*if(preg_match("/([012]?[1-9]|[12]0|3[01])\/(0?[1-9]|1[012])\/([0-9]{4})/", $data))
 	{
-		echo "<br>Data di nascita inserita: ";
+		echo "Data di nascita inserita: ";
 		echo $data;
+		echo "<br>";
 	}
 else
 	{
-		echo $errData = "Data non accettata, inserire una nuova data. <br>";
+		echo $errData = "<Data non accettata, inserire una nuova data. <br>";
 	}*/
 
+echo "Data di nascita inserita: ";
+echo $data;
+echo "<br>";
+
+echo "Nickname inserito: ";
 if(preg_match('/^[a-zA-Z0-9_]*$/',$nickname))
 	{
-		echo "<br>Nickname inserito: ";
-		echo $nickname;
+		echo $nickname, "<br>";
 	}
 else
 	{
-		echo $errNickname = "Caratteri non accettati, inserire un nuovo nickname. <br>";
+		echo $errNickname = "nickname invalido, inserire un nuovo nickname. <br>";
 	}
 	
-if(preg_match('/^[a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/',$email))
+	
+/*if(preg_match('/^[a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/',$email))
 	{
-		echo "<br>Email inserita: ";
+		echo "Email inserita: ";
 		echo $email;
+		echo "<br>";
 	}
 else
 	{
-		echo $errEmail ="Caratteri non accettati, inserire un nuovo nickname. <br>";
-	}
+		echo $errEmail ="Caratteri non accettati, inserire una nuova mail. <br>";
+	}*/
 	
-if(preg_match('/^[0-9]/',$cellulare))
+echo "Email: ";
+if (preg_match("/^[^@]+@[^@]+\.[a-z]{2,6}$/i", $email))
+    {
+		echo $email, "<br>";
+    }
+else
 	{
-		echo "<br>Numero di cellulare inserito: ";
-		echo $cellulare;
+		echo $errEmail ="email invalida, inserire una nuova mail. <br>";
+	}	
+	
+echo "Cellulare: ";
+if(preg_match("/^(([+]|00)39)?((3[1-6][0-9]))(\d{7})$/", $cellulare))
+	{
+		echo $cellulare, "<br>";
 	}
 else
 	{
-		echo $errCellulare = "Caratteri non accettati, inserire un nuovo numero. <br>";
+		echo $errCellulare = "numero invalido, inserire un nuovo numero. <br>";
 	}
 	
+echo "Password: ";
 if(preg_match('/^[a-zA-Z_0-9]/',$password))
 	{
-		echo "<br>Password inserita: ";
-		echo $password;
+		echo $password, "<br>";
 	}
 else
 	{
-		echo $errPassword = "Caratteri non accettati, inserire una nuova password. <br>";
+		echo $errPassword = "password invalida, inserire una nuova password. <br>";
 	}
 ?>
+
 </div>
-	<h2>VALIDAZIONE FORM</h2>
-	<p>* = Campi obbligatori da compilare</p>
+	<h2>VALIDAZIONE FORM<br>* = Campi obbligatori da compilare</h2>
 	<div class="sezione-login">
 	<form name="frmLogin" action="FormValidazione.php" method="POST">
-		NOME*: <input type="text" name="nome"><br><br>
-		COGNOME*: <input type="text" name="cognome"><br><br>
-		DATA*: <input type="date" name="data"><br><br>
-		NICKNAME*: <input type="text" name="nickname"><br><br>
-		EMAIL*: <input type="text" name="email"><br><br>
-		CELLULLARE: <input type="text" name="cellulare"><br><br>
-		PASSWORD*: <input type="password" name="password"><br><br>
-		<div class="tasto-submit"><input type="submit" value="Submit"></div>
+		*NOME: <input type="text" name="nome"><br><br>
+		*COGNOME: <input type="text" name="cognome"><br><br>
+		*DATA: <input type="date" name="data"><br><br>
+		*NICKNAME: <input type="text" name="nickname"><br><br>
+		*EMAIL: <input type="text" name="email"><br><br>
+		*CELLULLARE: <input type="text" name="cellulare"><br><br>
+		*PASSWORD: <input type="password" name="password"><br><br>
+		<div class="tasto-submit"><input type="submit" value="Submit" name="Invio"></div>
 	</form>		
 	</div>
 </body>
