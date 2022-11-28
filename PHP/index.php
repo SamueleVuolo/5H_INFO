eb840d8fc17cedf30c2b9625b56680a4f6a2799b
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2022-11-28 18:53:39 +0100

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -3,11 +3,12 @@
 	<title>Home Page</title>
 </head>
 <body>
+
 <h1>Home page - Vuolo Samuele</h1><hr/>
-<a href="info5H_vuolo/ES01 - Tavola Pitagorica/TavolaPit_Vuolo5H.php"><h2>Es01_Tavola</a><br/></h2>
-<a href="info5H_vuolo/ES02_A - Accesso a pagina riservata/PagRiservata.php"><h2>Es02_B_Login</a><br/></h2>
-<a href="info5H_vuolo/ES02_B - Login Unico/LoginUnico.php"><h2>Es02_A_LoginUnico</a><br/></h2>
-<a href="info5H_vuolo/ES03 - Validazione Form/FormValidazione.php"><h2>Es03_Validazione</a><br/></h2>
+<a href="info5H_vuolo/ES01-TavPit/TavolaPit_Vuolo5H.php"><h2>Es01 - Tavola Pitagorica</a><br/></h2>
+<a href="info5H_vuolo/ES02_A-Form/PagRiservata.php"><h2>Es02_A - Accesso a pagina riservata</a><br/></h2>
+<a href="info5H_vuolo/ES02_B-FormModificato/LoginUnico.php"><h2>Es02_B - Login unico</a><br/></h2>
+<a href="info5H_vuolo/ES03-ValidazioneForm/FormValidazione.php"><h2>Es03 - Validazione Form</a><br/></h2>
 <hr/>
 
 </body>
