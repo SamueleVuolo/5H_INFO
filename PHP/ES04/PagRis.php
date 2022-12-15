186755b0f1eaf8ea96fb0e3a3c08bd3483d43621
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2022-12-15 11:59:01 +0100

    Update PagRis.php

--- PHP/ES04/PagRis.php
+++ PHP/ES04/PagRis.php
@@ -1,1 +1,22 @@
+<html>
+<head>
+<title>ITCS Erasmo da Rotterdam</title>
+</head>
+<body>
+<?php
+include "fucntions.php";
 
+session_start(); //inizio della sessione
+	
+if(!isset($_SESSION["username"] && empty($_SESSION["username"])))
+	echo "Benvenuto nel sito!";
+else
+{
+	
+}
+?>
+
+
+
+</body>
+</html>
