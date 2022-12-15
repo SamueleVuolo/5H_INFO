d78e2b00ea1956111d51e5fa5549fe95d2c7b23a
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2022-12-15 11:09:10 +0100

    Create index_sessione.php

---
+++ PHP/ES04/index_sessione.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>Home Page</title>
+</head>
+<body>
+<h1>Home page</h1><hr/>
+<a href="info5H_vuolo/ES04 - Sessione Login/Login-PagRis/Login.php"><h2>Esegui Login</a><br/></h2>
+<a href="info5H_vuolo/ES04 - Sessione Login/Login-PagRis/PagRis.php"><h2>Vai alla pagina riservata</a><br/></h2>
+<hr/>
+</body>
+</html>
