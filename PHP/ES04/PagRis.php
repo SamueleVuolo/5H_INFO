580a1581bb829b72badae52a36887330ac3843fe
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2022-12-19 13:05:26 +0100

    Update PagRis.php

--- PHP/ES04/PagRis.php
+++ PHP/ES04/PagRis.php
@@ -4,19 +4,20 @@
 </head>
 <body>
 <?php
-include "fucntions.php";
+include "functions.php";
 
 session_start(); //inizio della sessione
 	
-if(!isset($_SESSION["username"] && empty($_SESSION["username"])))
+if(isset($_SESSION["username"] || empty($_SESSION["username"])))
+{
 	echo "Benvenuto nel sito!";
+	header("Location: PagRis.php");
+	session_destroy();
+}
 else
 {
 	
 }
 ?>
-
-
-
 </body>
 </html>
