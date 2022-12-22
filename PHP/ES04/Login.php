dfc84da01d2cb4e20296b3890880a655527fb1c7
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2022-12-22 20:44:10 +0100

    Update Login.php

--- PHP/ES04/Login.php
+++ PHP/ES04/Login.php
@@ -1,33 +1,31 @@
+<?php
+session_start();
+?>
 <html>
 <head>
 	<title>Accedi - ITCS ERASMO</title>
 	<link rel="stylesheet" href="stile.css?ts=<?=time()?>&quot">
 </head>
-
 <body>
 <?php
-	session_start();
 	if(isset($_POST["username"])) {
-		$username=$_POST["username"];
-		$password=$_POST["password"];
+		$username = $_POST["username"];
+		$password = $_POST["password"];
 		if($username!="admin" || $password!="pswd")
 			echo "<h2>Credenziali errate.</h2>";
 		else{
-			$_SESSION["username"]=$_POST["username"];
-			$_SESSION["password"]=$_POST["password"];
-			header("Location: PagRis.php");
+			$_SESSION["username"] = $_POST["username"];
+			$_SESSION["password"] = $_POST["password"];
+			header("location: PagRis.php");
 		}
 	}
 if(!isset($_SESSION["username"]))
 {?>
-	<div class="sezione-login">
-	<form name="frmLogin" action="PagRis.php" method="POST">
-		Username: <input type="text" name="username"><br>
-		Password: <input type="password" name="password"><br>
-		<input type="submit" value="Submit">
-	</form>
-	</div>
-	<hr/><?php
+	<form name="frmLogin" action="Login.php" method="post">
+		Username: <input type="text" name="username" placeholder="username"><br>
+		Password: <input type="password" name="password" placeholder="password"><br>
+		<input type="submit" value="Invio">
+	</form><hr/><?php
 }
 ?>
 <a href="index_sessione.php"><h2>Torna alla home page</a><br/></h2>
