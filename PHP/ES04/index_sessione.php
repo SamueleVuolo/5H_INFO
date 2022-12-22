13cc7b8eb299f7a918adf354befd502d6cde2749
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2022-12-22 20:45:22 +0100

    Update index_sessione.php

--- PHP/ES04/index_sessione.php
+++ PHP/ES04/index_sessione.php
@@ -1,11 +1,12 @@
 <html>
 <head>
 	<title>Home Page</title>
+	<link rel="stylesheet" href="stile.css?ts=<?=time()?>&quot">
 </head>
 <body>
 <h1>Home page</h1><hr/>
-<a href="info5H_vuolo/ES04 - Sessione Login/Login-PagRis/Login.php"><h2>Esegui Login</a><br/></h2>
-<a href="info5H_vuolo/ES04 - Sessione Login/Login-PagRis/PagRis.php"><h2>Vai alla pagina riservata</a><br/></h2>
+<a href="Login.php"><h2>Esegui Login</a><br/></h2>
+<a href="PagRis.php"><h2>Vai alla pagina riservata</a><br/></h2>
 <hr/>
 </body>
 </html>
