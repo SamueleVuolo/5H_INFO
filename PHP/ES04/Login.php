0eeaf650a523d076ee7ab7429c8ec6459d6f0683
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2022-12-22 11:48:16 +0100

    Update Login.php

--- PHP/ES04/Login.php
+++ PHP/ES04/Login.php
@@ -5,36 +5,28 @@
 </head>
 
 <body>
-<h2>Controllo credenziali</h2><hr/>
 <?php
-	if(isset($_POST["username"])) {
-		$usr=$_POST["username"];
-		$pwd=$_POST["password"];
-		if($usr!="admin" || $pwd!="password"){
-?>
-
-	<h3>Attenzione! Nome utente o password sbagliate.</br>
-	Accesso negato!</h3><hr/>
-
-<?php
-	} else {
-		echo "<h4>Benvenuto " . $usr . " nell'area del sito!</h4>";
+	session_start();
+	if(isset($_SESSION["username"])) {
+		$username=$_SESSION["username"];
+		$password=$_SESSION["password"];
+		if($username!="admin" || $password!="pswd")
+			echo "<h2>Credenziali errate.</h2>";
+		else
+			header("Location: PagRis.php");
 	}
-}	else {
-?>
+if(!isset($_SESSION["username"]))
+{?>
 	<div class="sezione-login">
 	<form name="frmLogin" action="PagRis.php" method="POST">
-		User name: <input type="text" name="username"><br>
+		Username: <input type="text" name="username"><br>
 		Password: <input type="password" name="password"><br>
 		<input type="submit" value="Submit">
 	</form>
 	</div>
-	<hr/>
-	
-<?php
+	<hr/><?php
 }
 ?>
-
 <a href="index_sessione.php"><h2>Torna alla home page</a><br/></h2>
 </body>
 </html>
