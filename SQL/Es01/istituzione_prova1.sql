5c2445d8c4879dce01f9eebfcc4a5be9a0c9f32e
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2023-01-12 11:58:04 +0100

    Update istituzione_prova1.sql

--- SQL/Es01/istituzione_prova1.sql
+++ SQL/Es01/istituzione_prova1.sql
@@ -3,12 +3,12 @@ CREATE DATABASE Istituzione;
 
 //creazione tabella scuola
 CREATE TABLE scuola(
-   	codice CHAR(10) PRIMARY KEY
+   	codice INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
 //creazione tabella studenti
 CREATE TABLE studenti(
     matricola CHAR(10),
     cognome CHAR(15) NOT NULL,
-    codice_scuola CHAR(10) REFERENCES scuola(codice)
+    codice_scuola INT REFERENCES scuola(codice)
 );
