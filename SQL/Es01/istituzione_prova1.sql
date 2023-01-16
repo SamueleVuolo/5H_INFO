62e8ae469f7c69f7bed6ecd7526f8d3b175eaf90
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2023-01-16 12:59:31 +0100

    Update istituzione_prova1.sql

--- SQL/Es01/istituzione_prova1.sql
+++ SQL/Es01/istituzione_prova1.sql
@@ -12,3 +12,26 @@ CREATE TABLE studenti(
     cognome CHAR(15) NOT NULL,
     codice_scuola INT REFERENCES scuola(codice)
 );
+
+//aggiungo un campo in studenti
+ALTER TABLE studenti ADD COLUMN(
+	classe CHAR(2)    
+);
+
+//creo un'istanza di studenti
+INSERT
+INTO studenti(matricola, cognome, codice_scuola, classe)
+VALUES ('H33KL9OE', 'Rambi', 'm4hhU7e', '5A');
+
+//modifico-aggiorno la tabella
+UPDATE studenti
+SET codice_scuola='17739164826';
+
+//creo un'istanza di scuola
+INSERT
+INTO scuola(codice)
+VALUES ('8344261984');
+
+//modifico-aggiorno la tabella
+UPDATE scuola
+SET codice='259352502';
