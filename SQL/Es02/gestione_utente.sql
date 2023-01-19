030315ace75354ad621ff06e07923e462f7310fa
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2023-01-19 11:57:37 +0100

    Update gestione_utente.sql

--- SQL/Es02/gestione_utente.sql
+++ SQL/Es02/gestione_utente.sql
@@ -1,1 +1,20 @@
+//crea database
 CREATE DATABASE gestione_utenti;
+
+//crea tabella utente
+CREATE TABLE utente(
+id INT NOT NULL AUTO_INCREMENT,
+username VARCHAR(64) NOT NULL,
+pswd VARCHAR(64) NOT NULL,
+PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=100;
+
+//inserimento dei campi
+INSERT 
+INTO utente(id, username, pswd)
+VALUES (NULL, 'Billy', 'provetta');
+
+//modifico tabella utente
+UPDATE utente
+SET pswd='blokka'
+WHERE id=100;
