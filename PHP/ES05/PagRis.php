060154dda6f7aca3de47ff7ffbb5610c98a13765
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2023-01-26 11:59:16 +0100

    Create PagRis.php

---
+++ PHP/ES05/PagRis.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>ITCS Erasmo da Rotterdam</title>
+<!--link rel="stylesheet" href="stile.css?ts=</?=time()?>&quot"-->
+</head>
+<body>
+<?php
+session_start(); //inizio della sessione
+if(!isset($_SESSION["username"]) && empty($_SESSION["username"]))
+{
+	echo "<h1>Effettuare prima il login.</h1>";?>
+	<a href="Login.php"><h1>Effettua il login</h1></a>
+<?php
+}
+else
+{
+	echo "<h1>Benvenuto nel sito!</h1>";
+?>
+	<a href="Logout.php"><h2>Esci</h2></a>
+<?php
+}
+?>
+</body>
+</html>
