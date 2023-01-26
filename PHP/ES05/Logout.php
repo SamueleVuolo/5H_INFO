41dbb13f0686f939deb266000999c33b5a2ed542
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2023-01-26 11:59:01 +0100

    Create Logout.php

---
+++ PHP/ES05/Logout.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Esci - ITCS ERASMO</title>
+	<!--link rel="stylesheet" href="stile.css?ts=</?=time()?>&quot"-->
+</head>
+<body>
+<?php 
+	session_start();
+	session_destroy();
+	echo "Logout avvenuto. Arrivederci";
+?>
+<hr/>
+<a href="Login.php"><h2>Accedi di nuovo</h2></a>
+<a href="index_sessione.php"><h2>Torna alla home</h2></a>
+</body>
+</html>
