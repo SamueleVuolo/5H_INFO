4d07c0dad4866c6e4c212349007901f1fe0b1cec
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2023-01-31 13:08:54 +0100

    Update Login.php

--- PHP/ES05/Login.php
+++ PHP/ES05/Login.php
@@ -2,53 +2,36 @@
 require 'function.php';
 
 session_start(); // Avvia la sessione php.
-
-define('DB_SERVER', 'localhost');
-define('DB_NAME', 'gestione_utenti');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
 ?>
 <?php
 
 if(!isset($_SESSION['login']))
 {
 	//il login non è ancora verificato
-	if(isset($_POST['Login'])
+	if(isset($_POST['Login']))
 	{
-		//verifichiamo che username e password sono presenti nel db
 		//connessione al database
-		connessionedb($_POST['username'], $_POST['password']);
-		
-		//interrogazione
-		//SELECT * FROM Utente WHERE username='' and password=''
-		$stmt = $pdo->prepare("SELECT * FROM utente WHERE username = :username AND password = :password");
-		
-		//controllo del record restituito
+		login();
 	}
 	else
 	{
-		//E' la prima volta che viene caricata la pagina
 		//Visualizzo il form
+?>
+		<form name="frmLogin" action="Login.php" method="post">
+			<label>Username <input type="text" name="username" /></label><br />
+			<label>Password <input type="password" name="password"/></label><br />
+			<input type="submit" value="Login" /><input type="reset" value="Cancel" />
+		</form>
+		<p>Non sei registrato? <a href="Registrazione.php"> Crea un account</a>.</p>
+<?php
 	}
 }
 else
 {
 	//L'utente ha già fatto il login
-	//Faccio redirect a riservata.php
-	//oppure visualizzo il link a riservata.php
+	echo "L'utente ha già effetuato il login <br>";
+	echo "<a href='homepage.php'>Home</a><br>";
+	echo "<a href='Logout.php'>Esci</a><br>";
 }
 ?>
 
-<html>
-<head><title>Login</title></head>
-<body>
-  <h3>Esegui Login</h3>
-  <?=$errmsg?>
-  <form action="Login.php" method="post">
-    <label>Username <input type="text" name="username" /></label><br />
-    <label>Password <input type="password" name="password"/></label><br />
-    <input type="submit" value="Login" /><input type="reset" value="Cancel" />
-  </form>
-  <p>Non sei registrato? <a href="Registrazione.php"> Crea un account</a>.</p>        
-</body>
-</html>
