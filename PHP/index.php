57cea85b00037ef8a5689f8421e79244a2087f38
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2023-01-31 13:08:37 +0100

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -9,7 +9,7 @@
 <a href="info5H_vuolo/ES02_B - Login Unico/LoginUnico.php"><h2>Es02_A_LoginUnico</a><br/></h2>
 <a href="info5H_vuolo/ES03 - Validazione Form/FormValidazione.php"><h2>Es03_Validazione</a><br/></h2>
 <a href="info5H_vuolo/ES04 - Sessione Login/index_sessione.php"><h2>Es04_Sessione</a><br/></h2>
-<a href="info5H_vuolo/ES05 - Connessione al Database/index_sessione.php"><h2>Es05_Accedi/Registrati</a><br/></h2>
+<a href="info5H_vuolo/ES05 - Connessione al Database/homepage.php"><h2>Es05_Accedi/Registrati</a><br/></h2>
 <hr/>
 
 </body>
