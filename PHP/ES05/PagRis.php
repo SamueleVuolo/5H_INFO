8db3b9398d253ae848ceeae5ab1de943ca5b0820
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2023-01-31 13:09:20 +0100

    Update PagRis.php

--- PHP/ES05/PagRis.php
+++ PHP/ES05/PagRis.php
@@ -6,7 +6,7 @@
 <body>
 <?php
 session_start(); //inizio della sessione
-if(!isset($_SESSION["username"]) && empty($_SESSION["username"]))
+if(!isset($_SESSION['login']))
 {
 	echo "<h1>Effettuare prima il login.</h1>";?>
 	<a href="Login.php"><h1>Effettua il login</h1></a>
@@ -14,7 +14,7 @@ if(!isset($_SESSION["username"]) && empty($_SESSION["username"]))
 }
 else
 {
-	echo "<h1>Benvenuto nel sito!</h1>";
+	echo "<h1>Benvenuto nella pagina riservata!</h1>";
 ?>
 	<a href="Logout.php"><h2>Esci</h2></a>
 <?php
