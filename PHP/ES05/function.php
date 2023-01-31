48a1b8e898c4cc48742ee9cfd37a3f23fc611477
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2023-01-31 13:09:50 +0100

    Update function.php

--- PHP/ES05/function.php
+++ PHP/ES05/function.php
@@ -1,11 +1,38 @@
 <?php
 
-function connessionedb() {
-  
+function login() {
+	
+	define('DB_SERVER', 'localhost');
+	define('DB_NAME', 'gestione_utenti');
+	define('DB_USER', 'root');
+	define('DB_PASSWORD', '');
+	
+	//passo variabili del login con POST
+	$username=$_POST['username'];
+	$pswd=$_POST['password'];
+	
+	//query per database
+	$pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+	
+	//interogazione database
+	$inter = "SELECT * FROM utente WHERE username = '$username' AND password = '$pswd'";
+	
+	//risultato interrogazione
+	$ris = $pdo->query($inter);
+	
+	if($ris->rowCount()>0){
+		//se vero
+		$_SESSION['login']=true;
+		echo "Benvenuto nella Homepage";
+		echo "<a href='PagRis.php'>Pagina Riservata</a>";
+	}
+	else
+		//se falso
+		$_SESSION['login']=false;
+		echo "Credenziali errate!";
 }
 
-function login($username, $password) {
-}
+/*function login($username, $password) {}*/
 
 
 
