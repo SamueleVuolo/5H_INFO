a5c6b6565474eca90fbb9e796eee31511a573b48
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2023-02-02 12:01:30 +0100

    Update istituzione_prova1.sql

--- SQL/Es01/istituzione_prova1.sql
+++ SQL/Es01/istituzione_prova1.sql
@@ -3,40 +3,61 @@ CREATE DATABASE Istituzione;
 
 //creazione tabella scuola
 CREATE TABLE scuola(
-   	codice INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+   	codice INT(65) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nome_scuola CHAR(64) NOT NULL
 );
+
+//creo le istanze per scuola
+INSERT INTO scuola(codice, nome_scuola) VALUES ('01', 'ITCS Erasmo da Rotterdam')
+INSERT INTO scuola(codice, nome_scuola) VALUES ('02', 'Russell')
+INSERT INTO scuola(codice, nome_scuola) VALUES ('03', 'Privo Levi')
+
 
 //creazione tabella studenti
 CREATE TABLE studenti(
-    matricola CHAR(10),
+    matricola CHAR(10) PRIMARY KEY,
     nome CHAR(15) NOT NULL,
     cognome CHAR(20) NOT NULL,
     data_nascita DATE NOT NULL,
-    codice_scuola INT NOT NULL,
+    classe CHAR(2) NOT NULL,
+    codice_scuola INT(65) NOT NULL,
     FOREIGN KEY(codice_scuola) REFERENCES scuola(codice)
     	ON UPDATE CASCADE
    	
 );
 
-//aggiungo un campo in studenti
-ALTER TABLE studenti ADD COLUMN(
-	classe CHAR(2)    
-);
+//creo le istanze per studenti
+INSERT INTO studenti(matricola, nome, cognome, data_nascita, classe, codice_scuola) VALUES ('101', 'Stefano', 'Befano', '2007/05/23', '2L', '01')
+INSERT INTO studenti(matricola, nome, cognome, data_nascita, classe, codice_scuola) VALUES ('425', 'Giacomo', 'Poretti', '2003/11/15', '4A', '02')
+INSERT INTO studenti(matricola, nome, cognome, data_nascita, classe, codice_scuola) VALUES ('58', 'Jovanni', 'Lavazza', '2006/09/13', '3F', '03')
 
-//creo un'istanza di studenti
-INSERT
-INTO studenti(matricola, cognome, codice_scuola, classe)
-VALUES ('H33KL9OE', 'Rambi', 'm4hhU7e', '5A');
 
-//modifico-aggiorno la tabella
-UPDATE studenti
-SET codice_scuola='17739164826';
+//visualizzo una riga
+SELECT * FROM studenti WHERE matricola='58';
 
-//creo un'istanza di scuola
-INSERT
-INTO scuola(codice)
-VALUES ('8344261984');
 
-//modifico-aggiorno la tabella
-UPDATE scuola
-SET codice='259352502';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
