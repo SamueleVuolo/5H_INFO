992264ebbae597ba2c234184c060a08972b26cd3
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2023-02-07 13:07:39 +0100

    Update Registrazione.php

--- PHP/ES05/Registrazione.php
+++ PHP/ES05/Registrazione.php
@@ -9,31 +9,26 @@
 <?php
   define('DB_SERVER', 'localhost');
   define('DB_NAME', 'gestione_utenti');
-  define('DB_USER', 'username');
-  define('DB_PASSWORD', 'pswd');
+  define('DB_USER', 'root');
+  define('DB_PASSWORD', '');
 
-  try {
-    //Connessione al database
-    $pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
-    //Imposto la modalità di gestione degli errori
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  } catch (PDOException $e) {
-    //Gestione dell'eccezione
-    echo "Errore di connessione: " . $e->getMessage();
-    exit;
-  }
+  if(!isset())
   
-  $pdo = new PDO("mysql:host=localhost;dbname=gestione_utenti", 'username', 'pswd');
-
-  //Chiusura della connessione
-  $pdo = null;
+  
+  
+  
+  
 ?>
 
 
-<form action="Login.php" method="post">
-   <label>Username <input type="text" name="username" /></label><br />
-   <label>Password <input type="password" name="password"/></label><br />
-   <input type="submit" value="Login" /><input type="reset" value="Cancel" />
+<form action="" method="post">
+   <label>Nome: <input type="text" name="nome" /></label><br />
+   <label>Cognome: <input type="text" name="cognome" /></label><br />
+   <label>Email: <input type="text" name="email" /></label><br />
+   <label>Data di nascita: <input type="date" name="data" /></label><br />
+   <label>Username: <input type="text" name="username" /></label><br />
+   <label>Password: <input type="password" name="password"/></label><br />
+   <input type="submit" value="Invio" />
 </form>        
 
 </body>
