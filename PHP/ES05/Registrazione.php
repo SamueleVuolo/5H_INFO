844ee1b8facf28c3c3ce7ec193229296d92a294f
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2023-02-17 00:11:58 +0100

    Update Registrazione.php

--- PHP/ES05/Registrazione.php
+++ PHP/ES05/Registrazione.php
@@ -1,35 +1,43 @@
+<?php
+session_start();
+require 'function.php';
+?>
+
 <html>
 <head>
-<title>Login</title>
+<title>Login</titlae>
 </head>
 
 <body>
-<h2>Crea un account</h2>
-
 <?php
+  //definizioni del db
   define('DB_SERVER', 'localhost');
   define('DB_NAME', 'gestione_utenti');
   define('DB_USER', 'root');
   define('DB_PASSWORD', '');
 
-  if(!isset())
-  
-  
-  
-  
-  
+  if(!isset($_SESSION['login']))
+	{
+	  if(!isset($_POST['Submit']))
+		{		  
 ?>
-
-
-<form action="" method="post">
-   <label>Nome: <input type="text" name="nome" /></label><br />
-   <label>Cognome: <input type="text" name="cognome" /></label><br />
-   <label>Email: <input type="text" name="email" /></label><br />
-   <label>Data di nascita: <input type="date" name="data" /></label><br />
-   <label>Username: <input type="text" name="username" /></label><br />
-   <label>Password: <input type="password" name="password"/></label><br />
-   <input type="submit" value="Invio" />
-</form>        
-
+		<h2>Creazione account</h2>
+		<form name="frmLogin" action="<?=$_SERVER['PHP_SELF']?>" method="post">
+		   <label>Nome: <input type="text" name="nome" /></label><br />
+		   <label>Cognome: <input type="text" name="cognome" /></label><br />
+		   <label>Email: <input type="text" name="email" /></label><br />
+		   <label>Data di nascita: <input type="date" name="nascita" /></label><br />
+		   <label>Username: <input type="text" name="username" /></label><br />
+		   <label>Password: <input type="password" name="password"/></label><br />
+		   <input type="submit" name="Submit" value="Invio" />
+		</form><?php     
+		}else 
+		 {
+			signup();
+			echo "Accedi alla pagina riservata";
+			echo "<a href='PagRis.php'>Pagina Riservata</a><br>";
+		 }
+	}
+?>
 </body>
 </html>
