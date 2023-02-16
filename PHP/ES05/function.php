4385b5fa733ee0b97217c32218b80d288a859e7e
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2023-02-17 00:11:33 +0100

    Update function.php

--- PHP/ES05/function.php
+++ PHP/ES05/function.php
@@ -1,12 +1,7 @@
 <?php
 
-function login() {
-	
-	/*define('DB_SERVER', 'localhost');
-	define('DB_NAME', 'gestione_utenti');
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', '');*/
-	
+function login()
+{	
 	//passo variabili del login con POST
 	$username=$_POST['username'];
 	$pswd=$_POST['password'];
@@ -15,8 +10,7 @@ function login() {
 	$pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
 	
 	//interogazione database
-	$inter = "SELECT * FROM utente WHERE username = '$username' AND password = '$pswd'";
-	
+	$inter = "SELECT * FROM utente WHERE username = '$username' AND pswd = '$pswd'";
 	//risultato interrogazione
 	$ris = $pdo->query($inter);
 	
@@ -27,28 +21,37 @@ function login() {
 		echo "<a href='PagRis.php'>Pagina Riservata</a>";
 	}
 	else
+	{
 		//se falso
 		$_SESSION['login']=false;
 		echo "Credenziali errate!";
+	}
 }
-
-/*function login($username, $password) {}*/
 
 function signup()
 {
-	$nome=$_POST['nome']
-	$cognome=$_POST['cognome']
-	$email=$_POST['email']
-	$nascita=$_POST['nascita']
-	$username=$_POST['username']
-	$pswd=$_POST['password']
+	$nome=$_POST['nome'];
+	$cognome=$_POST['cognome'];
+	$email=$_POST['email'];
+	$nascita=$_POST['nascita'];
+	$username=$_POST['username'];
+	$pswd=$_POST['password'];
 	
 	//query per database
 	$pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
 	
-	$regis = "INSERT INTO utente(id, nome, cognome, nascita, mail, username, pswd) VALUES(NULL, '$nome','$cognome','$email','$nascita','$username','$pswd')";
+	//inserisco i valori del POST
+	$regis = "INSERT INTO utente(id, nome, cognome, nascita, mail, username, pswd) VALUES(NULL, '$nome','$cognome','$nascita','$email','$username','$pswd')";
 	
+	if($pdo->query($regis)==true){
+            echo "Registrazione avvenuta ";
+        }else
+            echo "Errore. Impossibile registrarsi ";
+
+        //imposto la var di sessione a true
+        $_SESSION['login'] = true;
+		
+	//termino connessione al db
+	$pdo=null;
 }
-
-
-
+?>
