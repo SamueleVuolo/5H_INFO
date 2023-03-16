acd1a63f29baae522ad974837d217f65bca998c3
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2023-03-16 11:44:24 +0100

    Update PassReset.php

--- PHP/ES05_06_07/PassReset.php
+++ PHP/ES05_06_07/PassReset.php
@@ -1,28 +1,97 @@
 <?php
-session_start();
-require 'function.php';
-?>
-<html>
-<head>
-<title>Cambia password</title>
-</head>
 
-<body>
-<h2>Imposta nuova password</h2>
-
-<?php
-//definizioni del db
-  define('DB_SERVER', 'localhost');
-  define('DB_NAME', 'gestione_utenti');
-  define('DB_USER', 'root');
-  define('DB_PASSWORD', '');
-
-  if(!isset($_SESSION['login']))
-  {
-?>	
-	<form name="frmLogin" action="PassReset.php" method="POST">
-			<label>Username <input type="text" name="username" placeholder="username"></label><br/>
-			<label>Nuova Password <input type="password" name="password" placeholder="password"></label><br/>
-			<input type="submit" name="Login" />
-	</form>
+function login()
+{	
+	//passo variabili del login con POST
+	$username=$_POST['username'];
+	$pswd=$_POST['password'];
 	
+	//query per database
+	$pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);	
+	
+	//interogazione database
+	$inter = "SELECT * FROM utente WHERE username = '$username' AND pswd = '$pswd'";
+	//risultato interrogazione
+	$ris = $pdo->query($inter);
+	
+	if($ris->rowCount()>0){
+		//se vero
+		$_SESSION['login']=true;
+		echo "Benvenuto nella Homepage<br>";
+		echo "<a href='PagRis.php'>Pagina Riservata</a>";
+	}
+	else
+	{
+		//se falso
+		$_SESSION['login']=false;
+		echo "Credenziali errate!";
+	}
+}
+
+function signup()
+{
+	$nome=$_POST['nome'];
+	$cognome=$_POST['cognome'];
+	$email=$_POST['email'];
+	$nascita=$_POST['nascita'];
+	$username=$_POST['username'];
+	$pswd=$_POST['password'];
+	
+	//query per database
+	$pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+	
+	//inserisco i valori del POST
+	$regis = "INSERT INTO utente(id, nome, cognome, nascita, mail, username, pswd) VALUES(NULL, '$nome','$cognome','$nascita','$email','$username','$pswd')";
+	
+	if($pdo->query($regis)==true){
+            echo "Registrazione avvenuta ";
+        }else
+            echo "Errore. Impossibile registrarsi ";
+
+        //imposto la var di sessione a true
+        $_SESSION['login'] = true;
+		
+	//termino connessione al db
+	$pdo=null;
+}
+
+function resetpass()
+{
+	//passo le variabili con POST
+	$username = $_POST['username'];
+    $pswd = $_POST['password'];
+	$email = $_POST['email'];
+	
+	//query per database
+	$pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+	
+	//interogazione database
+	$inter = "SELECT * FROM utente WHERE username = '$username' AND mail = '$email'";
+	//risultato interrogazione
+	$ris = $pdo->query($inter);
+	if($ris->rowCount()>0)
+	{
+		//se vero
+		$_SESSION['reset']=true;
+			
+		$pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+		$pwreset = "UPDATE utente SET pswd='$pswd' WHERE username='$username'";
+		//controllo se la query è andata a buon fine
+		if($pdo->query($pwreset)==true)
+		{
+			echo "Password aggiornata ";
+		}else
+		echo "Impossibile aggiornare la password ";
+	}
+	else
+	{
+		//se falso
+		$_SESSION['reset']=false;
+		echo "Credenziali errate! <a href='PassReset.php'> Cambia Password</a><br>";
+	}
+	
+	//termino connessione al db
+	$pdo=null;
+	
+}
+?>
