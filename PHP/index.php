95d0029bd4e5a4e1d24c1c988f4ba1b156079d13
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2023-05-08 13:04:49 +0200

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -4,12 +4,13 @@
 </head>
 <body>
 <h1>Esercizi di Vuolo Samuele</h1><hr/>
-<a href="info5H_vuolo/ES01 - Tavola Pitagorica/TavolaPit_Vuolo5H.php"><h2>Es01_Tavola</a><br/></h2>
-<a href="info5H_vuolo/ES02_A - Accesso a pagina riservata/PagRiservata.php"><h2>Es02_B_Login</a><br/></h2>
-<a href="info5H_vuolo/ES02_B - Login Unico/LoginUnico.php"><h2>Es02_A_LoginUnico</a><br/></h2>
-<a href="info5H_vuolo/ES03 - Validazione Form/FormValidazione.php"><h2>Es03_Validazione</a><br/></h2>
-<a href="info5H_vuolo/ES04 - Sessione Login/index_sessione.php"><h2>Es04_Sessione</a><br/></h2>
-<a href="info5H_vuolo/ES05 - Connessione al Database/homepage.php"><h2>Es05_Accedi/Registrati</a><br/></h2>
+<a href="info5H_vuolo/ES01 - Tavola Pitagorica/TavolaPit_Vuolo5H.php"><h2>Es01 - Tavola</a><br/></h2>
+<a href="info5H_vuolo/ES02_A - Accesso a pagina riservata/PagRiservata.php"><h2>Es02_B - Login</a><br/></h2>
+<a href="info5H_vuolo/ES02_B - Login Unico/LoginUnico.php"><h2>Es02_A - LoginUnico</a><br/></h2>
+<a href="info5H_vuolo/ES03 - Validazione Form/FormValidazione.php"><h2>Es03 - Validazione</a><br/></h2>
+<a href="info5H_vuolo/ES04 - Sessione Login/index_sessione.php"><h2>Es04 - Sessione</a><br/></h2>
+<a href="info5H_vuolo/ES05_06_07 - Connessione al Database/homepage.php"><h2>Es05 - Accedi/Registrati</a><br/></h2>
+<a href="info5H_vuolo/ES08 - Show Database/show_table.php"><h2>Es08 - Mostra Database</a><br/></h2>
 <hr/>
 
 </body>
